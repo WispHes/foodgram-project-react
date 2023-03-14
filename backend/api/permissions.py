a9cc086370b982317e465707085bbe6037f641a9
Дмитрий Остапчук <wisphes@gmail.com>
2023-03-14 17:17:18 +0300

    commit

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class AuthorPermission(permissions.BasePermission):
-    """Авторские права на изменение и добавление"""
+    """Авторские права на изменение и добавление."""
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
